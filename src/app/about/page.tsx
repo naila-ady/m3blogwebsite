662125ccebba2fb67edf4930c1470448d44ae2e9
naila-ady <164941279+naila-ady@users.noreply.github.com>
2025-01-04 17:04:08 +0500

    .

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ const About = () => {
           <h2 className="  text-[60px] font-bold text-primary py-2 ">
           Ab<span  className=' text-[60px] font-bold text-gray-500'>out Blog Sphare</span>
           </h2>
-          <p className=" font-light text-[#fFFFFF] text-[40px] py-4 ">
+          <p className=" font-light text-gray-500 text-[40px] py-4 ">
           Welcome to The ED-Tech blog sphere – your ultimate source for insightful, engaging, and inspiring content!
            We aim to inform, inspire, and connect people through diverse
            topics spanning multiple education personels.</p>
@@ -20,7 +20,7 @@ const About = () => {
           Me<span  className=' text-[60px] font-bold text-gray-500'>et the Creator
           </span>
           </h2>
-          <p className="font-light text-[#ffffff] text-[40px] py-4 ">Hi, I'm Naila Adnan, the creator of
+          <p className="font-light text-gray-500 text-[40px] py-4 ">Hi, I'm Naila Adnan, the creator of
              Ed-Tech Blog. With a passion & love for exploring new ideas, I started this platform to share knowledge
               and ignite discussions on topics that matter. I'm committed of making  this Ed-Tech Blog your go-to destination for
                quality content.Outside of blogging, I enjoy coding, traveling, reading & cooking
@@ -30,7 +30,7 @@ const About = () => {
 
           </span>
           </h2>
-          <p className="  font-light text-[#fFFFFF] text-[40px] py-4 ">I value your input and love hearing
+          <p className="  font-light text-gray-500 text-[40px] py-4 ">I value your input and love hearing
              from you! Subscribe to our newsletter, share your thoughts, 
             or follow us on social media for the latest updates.</p>
           <div className="flex items-center flex-wrap pb-4 mt-3 w-full">
